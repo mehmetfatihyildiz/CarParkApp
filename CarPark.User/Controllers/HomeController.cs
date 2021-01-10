caca5ddda00a5af4bbbc7b340e4c74da078f3a6b
mhmtyldz <yldzmahmut0@gmail.com>
2021-01-10 20:56:44 +0300

    Source controla ilk etapta eklenmeyen yani ignore olan carpark.user'ı source controla ekledim

---
+++ CarPark.User/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CarPark.User.Models;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using Microsoft.Extensions.Localization;
+using System.Globalization;
+using System.Threading;
+
+namespace CarPark.User.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IStringLocalizer<HomeController> _localizer;
+
+        public HomeController(ILogger<HomeController> logger, IStringLocalizer<HomeController> localizer)
+        {
+            _logger = logger;
+            _localizer = localizer;
+        }
+
+        public IActionResult Index()
+        {
+            //var say_Hello_value = _localizer["Say_Hello"];
+
+            //var cultureInfo = CultureInfo.GetCultureInfo("en-US");
+            //Thread.CurrentThread.CurrentCulture = cultureInfo;
+            //Thread.CurrentThread.CurrentUICulture = cultureInfo;
+
+
+            //var say_Hello_value2 = _localizer["Say_Hello"];
+
+           
+
+            var customer = new Customer
+            {
+                Id = 2,
+                NameSurname = "Mahmut Yıldız",
+                Age = 28
+            };
+            _logger.LogError("Customer da bir hata oluştu {@customer}", customer);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
